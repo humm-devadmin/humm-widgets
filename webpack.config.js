9acab9f0a7af8c493d0a6e96c947f5ad38ecc8f2
Tao Wang <hellotaotao@gmail.com>
2019-08-12 10:34:17 +0930

    created landing-page-little widget

--- webpack.config.js
+++ webpack.config.js
@@ -11,6 +11,7 @@ module.exports = {
         // "au/content/scripts/more-info-general": "./src/au/more-info-general.ts",
         "au/content/scripts/price-info": "./src/au/price-info.ts",
         "au/content/scripts/landing-page": "./src/au/landing-page.ts",
+        "au/content/scripts/landing-page-little": "./src/au/landing-page-little.ts",
         "au/content/scripts/top-banner": "./src/au/more-info-small-slices.ts"
     },
 
