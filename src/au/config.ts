b88a4e0f8d3957af0041145842f7a3e253dc66b7
James <james.francis@flexicards.co.nz>
2020-07-15 15:08:17 +1200

    feat(pricing): dynamic big things pricing via API call v1 and v2 widget

--- src/au/config.ts
+++ src/au/config.ts
@@ -5,10 +5,10 @@
 //   baseContentUrl = 'https://YOUR-URL'
 
 export class Config {
-    public static baseContentUrl = 'https://widgets.shophumm.com.au';    // for remote deploy
+    // public static baseContentUrl = 'https://widgets.shophumm.com.au';    // for remote deploy
     // public static baseContentUrl = 'https://s3-ap-southeast-2.amazonaws.com/widgets.oxipay.com.au';    // for remote deploy
     // public static baseContentUrl = 'https://s3-ap-southeast-2.amazonaws.com/widgets.shophumm.com.au/dist/au';    // for remote demo
-    // public static baseContentUrl = './dist/au';       // for local testing
+    public static baseContentUrl = './dist/au';       // for local testing
 
     // price-info modal
     public static priceInfoModalId = 'humm-price-info-modal';
@@ -17,6 +17,10 @@ export class Config {
     // price-info-more modal
     public static priceInfoMoreModalId = 'humm-price-info-more-modal';
     public static priceInfoMoreUrl = Config.baseContentUrl + '/content/html/priceInfoMoreModal.html';
+
+    // price-info-api modal
+    public static priceInfoAPIModalId = 'humm-price-info-api-modal';
+    public static priceInfoAPIModalUrl = Config.baseContentUrl + '/content/html/priceInfoAPIModal.html';
 
     //price-info-ent modal
     public static priceInfoV2ModalId = 'humm-price-info-v2-modal';
@@ -29,4 +33,7 @@ export class Config {
     // top-banner modal
     public static hummBannerTopModalId = Config.moreInfoModalId;
     public static hummBannerTopModalUrl = Config.moreInfoUrlNew;
+
+    // price info api
+    public static priceInfoAPIUrl = 'https://buyerapi.shophumm.com.au/api';
 }
